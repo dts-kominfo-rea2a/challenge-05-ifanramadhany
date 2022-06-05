c9010304a952f86f677c24651e5a93ebc255d124
Ifan Ramadhany <ifanpersie14@gmail.com>
2022-06-05 16:14:03 +0700

    challenge-05-done/fix-01

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const sorter = (names, callbackSorting) => {
   let sortedNames = callbackSorting(names)
   let result = []
   for (let i = 0; i < sortedNames.length; i++) {
-    result.push(`${i+1} ${sortedNames[i]}`)
+    result.push(`${i+1}. ${sortedNames[i]}`)
   }
   return result
 };
